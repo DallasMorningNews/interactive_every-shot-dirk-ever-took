f4d3ab404564e154580c7d82e4fe962609e1067c
jdhancock88 <jdhancock@gmail.com>
2018-10-05 09:08:11 -0500

    Adds scraper files to git.

---
+++ src/assets/seven-footers.py
@@ -0,0 +1,290 @@
+# seven foot or taller, minimum 400 field goals in a season
+
+[
+ {
+    season: "1999-2000",
+    players: [
+        {name: "Shaquille O'Neal", player_id: 406, total_attempts: 0, avg_distance: 0},
+        {name: "David Robinson", player_id: 764, total_attempts: 0, avg_distance: 0},
+        {name: "Dikembe Mutombo", player_id: 87, total_attempts: 0, avg_distance: 0},
+        {name: "Dirk Nowitzki", player_id: 1717, total_attempts: 0, avg_distance: 0},
+        {name: "Vlade Divac", player_id: 124, total_attempts: 0, avg_distance: 0},
+        {name: "Arvydas Sabonis", player_id: 717, total_attempts: 0, avg_distance: 0},
+        {name: "Shawn Bradley", player_id: 762, total_attempts: 0, avg_distance: 0},
+        {name: "Patrick Ewing", player_id: 121, total_attempts: 0, avg_distance: 0},
+        {name: "Rik Smits", player_id: 22, total_attempts: 0, avg_distance: 0},
+        {name: "Rasho Nesterovic", player_id: 1725, total_attempts: 0, avg_distance: 0},
+        {name: "Matt Geiger", player_id: 761, total_attempts: 0, avg_distance: 0},
+        {name: "Kevin Willis", player_id: 788, total_attempts: 0, avg_distance: 0},
+        {name: "Bryant Reeves", player_id: 735 total_attempts: 0, avg_distance: 0},
+        {name: "Michael Olowokandi", player_id: 1709, total_attempts: 0, avg_distance: 0}
+        {name: "Hakeem Olajuwon", player_id: 165, total_attempts: 0, avg_distance: 0}
+
+    ]
+ },
+ {
+    season: "2000-01",
+    players: [
+        {name: "Shaquille O'Neal", player_id: 406, total_attempts: 0, avg_distance: 0},
+        {name: "David Robinson", player_id: 764, total_attempts: 0, avg_distance: 0},
+        {name: "Dikembe Mutombo", player_id: 87, total_attempts: 0, avg_distance: 0},
+        {name: "Dirk Nowitzki", player_id: 1717, total_attempts: 0, avg_distance: 0},
+        {name: "Vlade Divac", player_id: 124, total_attempts: 0, avg_distance: 0},
+        {name: "Arvydas Sabonis", player_id: 717, total_attempts: 0, avg_distance: 0},
+        {name: "Shawn Bradley", player_id: 762, total_attempts: 0, avg_distance: 0},
+        {name: "Patrick Ewing", player_id: 121, total_attempts: 0, avg_distance: 0},
+        {name: "Kevin Willis", player_id: 788, total_attempts: 0, avg_distance: 0},
+        {name: "Bryant Reeves", player_id: 735 total_attempts: 0, avg_distance: 0},
+        {name: "Michael Olowokandi", player_id: 1709, total_attempts: 0, avg_distance: 0},
+        {name: "Hakeem Olajuwon", player_id: 165, total_attempts: 0, avg_distance: 0}
+    ]
+ },
+ {
+    season: "2001-02",
+    players: [
+        {name: "Shaquille O'Neal", player_id: 406, total_attempts: 0, avg_distance: 0},
+        {name: "David Robinson", player_id: 764, total_attempts: 0, avg_distance: 0},
+        {name: "Dikembe Mutombo", player_id: 87, total_attempts: 0, avg_distance: 0},
+        {name: "Dirk Nowitzki", player_id: 1717, total_attempts: 0, avg_distance: 0},
+        {name: "Vlade Divac", player_id: 124, total_attempts: 0, avg_distance: 0},
+        {name: "Michael Olowokandi", player_id: 1709, total_attempts: 0, avg_distance: 0},
+        {name: "Pau Gasol", player_id: 2200, total_attempts: 0, avg_distance: 0},
+        {name: "Rasho Nesterovic", player_id: 1725, total_attempts: 0, avg_distance: 0},
+        {name: "Zydrunas Ilgauskas", player_id: 980, total_attempts: 0, avg_distance: 0},
+        {name: "Chris Mihm", player_id: 2036, total_attempts: 0, avg_distance: 0},
+        {name: "Hakeem Olajuwon", player_id: 165, total_attempts: 0, avg_distance: 0},
+        {name: "Todd MacCulloch", player_id: 1928, total_attempts: 0, avg_distance: 0},
+        {name: "Jake Tsakalidis", player_id: 2054, total_attempts: 0, avg_distance: 0}
+    ]
+ },
+ {
+    season: "2002-03",
+    players: [
+        {name: "Shaquille O'Neal", player_id: 406, total_attempts: 0, avg_distance: 0},
+        {name: "Dirk Nowitzki", player_id: 1717, total_attempts: 0, avg_distance: 0},
+        {name: "Vlade Divac", player_id: 124, total_attempts: 0, avg_distance: 0},
+        {name: "Pau Gasol", player_id: 2200, total_attempts: 0, avg_distance: 0},
+        {name: "Rasho Nesterovic", player_id: 1725, total_attempts: 0, avg_distance: 0},
+        {name: "Zydrunas Ilgauskas", player_id: 980, total_attempts: 0, avg_distance: 0},
+        {name: "Yao Ming", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Tyson Chandler", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Vladimir Stepania", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Michael Olowokandi", player_id: 1709, total_attempts: 0, avg_distance: 0},
+        {name: "David Robinson", player_id: 764, total_attempts: 0, avg_distance: 0},
+        {name: "Eddy Curry", player_id: 2201, total_attempts: 0, avg_distance: 0}
+    ]
+ },
+ {
+    season: "2003-04",
+    players: [
+        {name: "Shaquille O'Neal", player_id: 406, total_attempts: 0, avg_distance: 0},
+        {name: "Dirk Nowitzki", player_id: 1717, total_attempts: 0, avg_distance: 0},
+        {name: "Vlade Divac", player_id: 124, total_attempts: 0, avg_distance: 0},
+        {name: "Pau Gasol", player_id: 2200, total_attempts: 0, avg_distance: 0},
+        {name: "Rasho Nesterovic", player_id: 1725, total_attempts: 0, avg_distance: 0},
+        {name: "Zydrunas Ilgauskas", player_id: 980, total_attempts: 0, avg_distance: 0},
+        {name: "Yao Ming", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Mark Blount", player_id: 1548, total_attempts: 0, avg_distance: 0},
+        {name: "Greg Ostertag", player_id: 731, total_attempts: 0, avg_distance: 0},
+        {name: "Chris Kaman", player_id: 2549, total_attempts: 0, avg_distance: 0},
+        {name: "Eddy Curry", player_id: 2201, total_attempts: 0, avg_distance: 0}
+    ]
+ },
+ {
+    season: "2004-05",
+    players: [
+        {name: "Shaquille O'Neal", player_id: 406, total_attempts: 0, avg_distance: 0},
+        {name: "Dirk Nowitzki", player_id: 1717, total_attempts: 0, avg_distance: 0},
+        {name: "Zydrunas Ilgauskas", player_id: 980, total_attempts: 0, avg_distance: 0},
+        {name: "Yao Ming", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Pau Gasol", player_id: 2200, total_attempts: 0, avg_distance: 0},
+        {name: "Eddy Curry", player_id: 2201, total_attempts: 0, avg_distance: 0},
+        {name: "Mark Blount", player_id: 1548, total_attempts: 0, avg_distance: 0},
+        {name: "Chris Mihm", player_id: 2036, total_attempts: 0, avg_distance: 0},
+        {name: "Chris Kaman", player_id: 2549, total_attempts: 0, avg_distance: 0},
+        {name: "Rasho Nesterovic", player_id: 1725, total_attempts: 0, avg_distance: 0},
+        {name: "Tyson Chandler", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Primoz Brezec", player_id: 2056, total_attempts: 0, avg_distance: 0},
+        {name: "Nenad Krstic", player_id: 2420, total_attempts: 0, avg_distance: 0},
+        {name: "Jason Collins", player_id: 2215, total_attempts: 0, avg_distance: 0},
+        {name: "Brendan Haywood", player_id: 2217, total_attempts: 0, avg_distance: 0}
+    ]
+ },
+ {
+    season: "2005-06",
+    players: [
+        {name: "Dirk Nowitzki", player_id: 1717, total_attempts: 0, avg_distance: 0},
+        {name: "Zydrunas Ilgauskas", player_id: 980, total_attempts: 0, avg_distance: 0},
+        {name: "Yao Ming", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Pau Gasol", player_id: 2200, total_attempts: 0, avg_distance: 0},
+        {name: "Shaquille O'Neal", player_id: 406, total_attempts: 0, avg_distance: 0},
+        {name: "Nenad Krstic", player_id: 2420, total_attempts: 0, avg_distance: 0},
+        {name: "Primoz Brezec", player_id: 2056, total_attempts: 0, avg_distance: 0},
+        {name: "Mark Blount", player_id: 1548, total_attempts: 0, avg_distance: 0},
+        {name: "Chris Mihm", player_id: 2036, total_attempts: 0, avg_distance: 0},
+        {name: "Chris Kaman", player_id: 2549, total_attempts: 0, avg_distance: 0},
+        {name: "Eddy Curry", player_id: 2201, total_attempts: 0, avg_distance: 0},
+        {name: "Brendan Haywood", player_id: 2217, total_attempts: 0, avg_distance: 0},
+        {name: "Andrew Bogut", player_id: 101106, total_attempts: 0, avg_distance: 0}
+    ]
+ },
+ {
+    season: "2006-07",
+    players: [
+        {name: "Shaquille O'Neal", player_id: 406, total_attempts: 0, avg_distance: 0},
+        {name: "Dirk Nowitzki", player_id: 1717, total_attempts: 0, avg_distance: 0},
+        {name: "Zydrunas Ilgauskas", player_id: 980, total_attempts: 0, avg_distance: 0},
+        {name: "Yao Ming", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Pau Gasol", player_id: 2200, total_attempts: 0, avg_distance: 0},
+        {name: "Eddy Curry", player_id: 2201, total_attempts: 0, avg_distance: 0},
+        {name: "Mark Blount", player_id: 1548, total_attempts: 0, avg_distance: 0},
+        {name: "Chris Kaman", player_id: 2549, total_attempts: 0, avg_distance: 0},
+        {name: "Rasho Nesterovic", player_id: 1725, total_attempts: 0, avg_distance: 0},
+        {name: "Tyson Chandler", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Andrew Bogut", player_id: 101106, total_attempts: 0, avg_distance: 0},
+        {name: "Andrea Bargnani", player_id: 200745, total_attempts: 0, avg_distance: 0},
+        {name: "Darko Milicic", player_id: 2545, total_attempts: 0, avg_distance: 0},
+        {name: "Andrew Bynum", player_id: 101115, total_attempts: 0, avg_distance: 0},
+        {name: "Johan Petro", player_id: 101130, total_attempts: 0, avg_distance: 0}
+    ]
+ },
+ {
+    season: "2007-08",
+    players: [
+        {name: "Shaquille O'Neal", player_id: 406, total_attempts: 0, avg_distance: 0},
+        {name: "Dirk Nowitzki", player_id: 1717, total_attempts: 0, avg_distance: 0},
+        {name: "Zydrunas Ilgauskas", player_id: 980, total_attempts: 0, avg_distance: 0},
+        {name: "Yao Ming", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Pau Gasol", player_id: 2200, total_attempts: 0, avg_distance: 0},
+        {name: "Eddy Curry", player_id: 2201, total_attempts: 0, avg_distance: 0},
+        {name: "Mark Blount", player_id: 1548, total_attempts: 0, avg_distance: 0},
+        {name: "Chris Kaman", player_id: 2549, total_attempts: 0, avg_distance: 0},
+        {name: "Rasho Nesterovic", player_id: 1725, total_attempts: 0, avg_distance: 0},
+        {name: "Tyson Chandler", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Andrew Bogut", player_id: 101106, total_attempts: 0, avg_distance: 0},
+        {name: "Andrea Bargnani", player_id: 200745, total_attempts: 0, avg_distance: 0},
+        {name: "Darko Milicic", player_id: 2545, total_attempts: 0, avg_distance: 0},
+        {name: "Yi Jianlian", player_id: 201146, total_attempts: 0, avg_distance: 0},
+        {name: "Johan Petro", player_id: 101130, total_attempts: 0, avg_distance: 0},
+        {name: "Brendan Haywood", player_id: 2217, total_attempts: 0, avg_distance: 0}
+    ]
+ },
+ {
+    season: "2008-09",
+    players: [
+        {name: "Shaquille O'Neal", player_id: 406, total_attempts: 0, avg_distance: 0},
+        {name: "Dirk Nowitzki", player_id: 1717, total_attempts: 0, avg_distance: 0},
+        {name: "Zydrunas Ilgauskas", player_id: 980, total_attempts: 0, avg_distance: 0},
+        {name: "Yao Ming", player_id: 2397, total_attempts: 0, avg_distance: 0},
+        {name: "Pau Gasol", player_id: 2200, total_attempts: 0, avg_distance: 0},
+        {name: "Brook Lopez", player_id: 201572, total_attempts: 0, avg_distance: 0},
+        {name: "Rasho Nesterovic", player_id: 1725, total_attempts: 0, avg_distance: 0},
+        {name: "Roy Hibbert", player_id: 201579, total_attempts: 0, avg_distance: 0},
+        {name: "Spencer Hawes", player_id: 201150, total_attempts: 0, avg_distance: 0},
+        {name: "Andrea Bargnani", player_id: 200745, total_attempts: 0, avg_distance: 0},
+        {name: "Yi Jianlian", player_id: 201146, total_attempts: 0, avg_distance: 0},
+        {name: "Andrew Bynum", player_id: 101115, total_attempts: 0, avg_distance: 0},
+        {name: "Nenad Krstic", player_id: 2420, total_attempts: 0, avg_distance: 0},
+        {name: "Marc Gasol", player_id: 201188, total_attempts: 0, avg_distance: 0}
+    ]
+ },
+ {
+    season: "2009-10",
+    players: [
+        {
+            name: "",
+            player_id: 0,
+            total_attempts: 0,
+            avg_distance: 0
+        },
+    ]
+ },
+ {
+    season: "2010-11",
+    players: [
+        {
+            name: "",
+            player_id: 0,
+            total_attempts: 0,
+            avg_distance: 0
+        },
+    ]
+ },
+ {
+    season: "2011-12",
+    players: [
+        {
+            name: "",
+            player_id: 0,
+            total_attempts: 0,
+            avg_distance: 0
+        },
+    ]
+ },
+ {
+    season: "2012-13",
+    players: [
+        {
+            name: "",
+            player_id: 0,
+            total_attempts: 0,
+            avg_distance: 0
+        },
+    ]
+ },
+ {
+    season: "2013-14",
+    players: [
+        {
+            name: "",
+            player_id: 0,
+            total_attempts: 0,
+            avg_distance: 0
+        },
+    ]
+ },
+ {
+    season: "2014-15",
+    players: [
+        {
+            name: "",
+            player_id: 0,
+            total_attempts: 0,
+            avg_distance: 0
+        },
+    ]
+ },
+ {
+    season: "2015-16",
+    players: [
+        {
+            name: "",
+            player_id: 0,
+            total_attempts: 0,
+            avg_distance: 0
+        },
+    ]
+ },
+ {
+    season: "2016-17",
+    players: [
+        {
+            name: "",
+            player_id: 0,
+            total_attempts: 0,
+            avg_distance: 0
+        },
+    ]
+ },
+ {
+    season: "2017-18",
+    players: [
+        {
+            name: "",
+            player_id: 0,
+            total_attempts: 0,
+            avg_distance: 0
+        },
+    ]
+ }
+]
